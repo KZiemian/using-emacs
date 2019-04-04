95307329e901cf410e1843adb4f618c3013519a4
Kamil Ziemian <kziemianfvt@gmail.com>
2019-04-04 14:41:43 +0200

    Don't needed `progn` removed
    I think that is more idiomatic way to use elisp and use-package.

--- init.el
+++ init.el
@@ -64,9 +64,8 @@
 (use-package ace-window
   :ensure t
   :init
-  (progn
-    (global-set-key [remap other-window] 'ace-window)
-    (custom-set-faces
-     '(aw-leading-char-face
-       ((t (:inherit ace-jump-face-foreground :height 3.0))))) 
-    ))
+  (global-set-key [remap other-window] 'ace-window)
+  (custom-set-faces
+   '(aw-leading-char-face
+     ((t (:inherit ace-jump-face-foreground :height 3.0))))
+   ))
